f4e987f9e39c42b0f5e43dcc165e4b8770d972dc
Christopher Tokar <christopher.tokar@gmail.com>
2019-03-15 21:31:25 +0100

    basic table functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import Grid from './Grid';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Grid columnDefs={[{
+          headerName: "Id", field: "id"
+        },{
+          headerName: "CompanyName", field: "CompanyName"
+        }, {
+          headerName: "Address", field: "Address"
+        }, {
+          headerName: "City", field: "City"
+        }, {
+          headerName: "State", field: "State"
+        }, {
+          headerName: "Zipcode", field: "Zipcode"
+        }, {
+          headerName: "Phone", field: "Phone"
+        }, {
+          headerName: "Fax", field: "Fax"
+        }, {
+          headerName: "Website", field: "Website"
+        }]} />
       </div>
     );
   }
