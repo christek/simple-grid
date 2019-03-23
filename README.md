d69c299d76ccb4baf7703862ce782d5847a7d896
Christopher Tokar <christopher.tokar@gmail.com>
2019-03-23 14:37:08 +0100

    manually changing page

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 ## Running web server
 
-json-server --watch db.json --port 3004
+json-server --watch db.json --port 3004```````
--- src/Grid.js
+++ src/Grid.js
@@ -2,32 +2,36 @@ import React, { Component } from 'react';
 import './Grid.css';
 
 export default class Grid extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        error: null,
-        isLoaded: false,
-        items: [],
-        page: 1,
-      };
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      items: [],
+      page: 1,
+      totalCount: 0,
+    };
 
-      this.incrementPagination = this.incrementPagination.bind(this);
-      this.decrementPagination = this.decrementPagination.bind(this);
-      this.fetchData = this.fetchData.bind(this);
-    }
+    this.incrementPagination = this.incrementPagination.bind(this);
+    this.decrementPagination = this.decrementPagination.bind(this);
+    this.changePagination = this.changePagination.bind(this);
+    this.fetchData = this.fetchData.bind(this);
+  }
 
-    fetchData() {
-      console.log(this.state.page);
-      fetch(`http://localhost:3004/data?_page=${this.state.page}`).then((res) => {
-        console.log(this.state.page);
-        return res;
-      })
+  fetchData() {
+    fetch(`http://localhost:3004/data?_page=${this.state.page}`).then((res) => {
+      console.log(res.headers.get( 'X-Total-Count' ));
+      this.setState({
+        totalCount: parseInt(res.headers.get( 'X-Total-Count' ))
+      });
+      return res;
+    })
       .then(res => res.json())
       .then(
         (result) => {
           this.setState({
             isLoaded: true,
-            items: result
+            items: result,
           });
         },
         // Note: it's important to handle errors here
@@ -39,53 +43,72 @@ export default class Grid extends React.Component {
             error
           });
         }
-      ).then(() => {
-        console.log('done fetching');
-      })
-    }
+      )
+  }
 
-    incrementPagination() {
-      this.setState({page: this.state.page+1});
-      console.log('set state done');
-      this.fetchData();
-    }
-
-    decrementPagination() {
-      this.setState({page: this.state.page-1});
-      this.fetchData();
-    }
-  
-    componentDidMount() {
-      this.fetchData();
-    }
-  
-    render() {
-      const { error, isLoaded, items } = this.state;
-      if (error) {
-        return <div>Error: {error.message}</div>;
-      } else if (!isLoaded) {
-        return <div>Loading...</div>;
-      } else {
-        return (
-          <table>
-            <tr>
-            {this.props.columnDefs.map(item => (
-                <th>{item.headerName}</th>
-            ))}
-            </tr>
-            {items.map(item => (
-              <tr key={item.id}>
-                {Object.keys(item).map(key => (<td>{item[key]}</td>))}
-              </tr>
-            ))}
-            <tr>
-               <td>
-                 <input type="button" name="name" value="-" onClick={this.decrementPagination} />
-                 <input type="button" name="name" value="+" onClick={this.incrementPagination} />
-               </td>
-            </tr>
-          </table>
-        );
-      }
+  incrementPagination() {
+    if (this.state.page * 10 < (Math.ceil((this.state.totalCount+1)/10)*10)) {
+      this.setState({ page: this.state.page + 1 }, () => {
+        this.fetchData();
+      });
     }
   }
+
+  logKey(event) {
+    console.log(event.target.key); // combine this into one object
+    console.log(event.target.id);
+  }
+
+  decrementPagination() {
+    if (this.state.page > 1) {
+      this.setState({ page: this.state.page - 1 }, () => {
+        this.fetchData();
+      });
+    }
+  }
+
+  changePagination(event) {
+    if (!(parseInt(event.target.value) * 10 <= (Math.ceil((this.state.totalCount+1)/10)*10))) return;
+    if (event.target.value === '') return; 
+    this.setState({ page: parseInt(event.target.value) }, () => {
+      this.fetchData();
+    });
+  }
+
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  render() {
+    const { error, isLoaded, items } = this.state;
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <table>
+          <tbody>
+          <tr>
+            {this.props.columnDefs.map(item => (
+              <th key={item.headerName}>{item.headerName}</th>
+            ))}
+          </tr>
+          {items.map(item => (
+            <tr key={item.id}>
+              {Object.keys(item).map(key => (<td key={key} onClick={this.logKey} id={item.id}>{item[key]}</td>))}
+            </tr>
+          ))}
+          <tr>
+            <td>
+              <input type="button" name="increment" value="-" onClick={this.decrementPagination} />
+              <input type="text" name="pagination" value={this.state.page} onChange={this.changePagination} />
+              <input type="button" name="decrement" value="+" onClick={this.incrementPagination} />
+            </td>
+          </tr>
+          </tbody>
+        </table>
+      );
+    }
+  }
+}
