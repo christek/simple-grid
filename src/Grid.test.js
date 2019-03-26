cb4453af894ef07381b202da254ec7e691b73af0
Christopher Tokar <christopher.tokar@gmail.com>
2019-03-26 17:35:00 +0100

    added unit test checking for fetching

--- src/Grid.test.js
+++ src/Grid.test.js
@@ -6,46 +6,61 @@ import Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() });
 
-import { shallow, mount, render } from 'enzyme';
-
-
-
-
+import { shallow } from 'enzyme';
 import Grid from './Grid';
-import ColumnSorter from './ColumnSorter';
 
 
 it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<Grid />, div);
     ReactDOM.unmountComponentAtNode(div);
-  });
-  
+});
 
-test('desc', () => {
-  // Render a checkbox with label in the document
-  const grid = shallow(<Grid columnDefs={[{
-    headerName: "Id", field: "id"
-  },{
-    headerName: "CompanyName", field: "CompanyName"
-  }, {
-    headerName: "Address", field: "Address"
-  }, {
-    headerName: "City", field: "City"
-  }, {
-    headerName: "State", field: "State"
-  }, {
-    headerName: "Zipcode", field: "Zipcode"
-  }, {
-    headerName: "Phone", field: "Phone"
-  }, {
-    headerName: "Fax", field: "Fax"
-  }, {
-    headerName: "Website", field: "Website"
-  }]}
-  pageSize={10}
-  sortCol={"id"}
-  />);
 
-  expect(grid.find(ColumnSorter)).to.have.lengthOf(9);
+it('shows loading message by default', () => {
+    // Render a checkbox with label in the document
+    const grid = shallow(<Grid columnDefs={[{
+        headerName: "Id", field: "id"
+    }, {
+        headerName: "CompanyName", field: "CompanyName"
+    }, {
+        headerName: "Address", field: "Address"
+    }, {
+        headerName: "City", field: "City"
+    }, {
+        headerName: "State", field: "State"
+    }, {
+        headerName: "Zipcode", field: "Zipcode"
+    }, {
+        headerName: "Phone", field: "Phone"
+    }, {
+        headerName: "Fax", field: "Fax"
+    }, {
+        headerName: "Website", field: "Website"
+    }]}
+        pageSize={10}
+        sortCol={"id"}
+    />);
+    expect(grid.html()).toEqual('<div>Loading...</div>');
+});
+
+
+it('when rendering Grid, fetch is called once', done => { // 1
+    // https://medium.com/@rishabhsrao/mocking-and-testing-fetch-with-jest-c4d670e2e167
+    const mockSuccessResponse = {};
+    const mockJsonPromise = Promise.resolve(mockSuccessResponse);
+    const mockFetchPromise = Promise.resolve({
+        json: () => mockJsonPromise,
+    });
+    jest.spyOn(global, 'fetch').mockImplementation(() => mockFetchPromise);
+
+    const wrapper = shallow(<Grid />);
+
+    expect(global.fetch).toHaveBeenCalledTimes(1);
+    //expect(global.fetch).toHaveBeenCalledWith('http://localhost:3004/data?_page=1&_limit=undefined&_sort=undefined');
+
+    process.nextTick(() => { // 6
+        global.fetch.mockClear(); // 7
+        done(); // 8
+    });
 });
